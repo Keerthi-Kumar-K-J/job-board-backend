8fa5f0007068bc6bd3d5496c41bb4f95d8cb8b3d
Keerthi Kumar K J <keerthidonkumar@gmail.com>
2025-03-20 05:25:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const sequelize = require('./models/index'); // Import Sequelize setup
 const app = express();
 
-const PORT = process.env.PORT || 3000;
 
 // Home Route
 app.get('/', (req, res) => {
